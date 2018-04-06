3dbaea6b4e5b5bf7489b85f56666d8bc624f3f6c
bredele <olivier.wietrich@gmail.com>
2018-04-06 13:03:48 -0600

    Create multiple groups

--- test/array-classify.js
+++ test/array-classify.js
@@ -12,13 +12,21 @@ test('group objects in an array given a sorting callback', assert => {
     {name: 'beep', value: 'boop'},
     {name: 'foo', value: 'world'},
     {name: 'bar', value: 'beep'},
+    {name: 'john', value: 'doe'},
+    {name: 'beep', value: 'beep'},
+    {name: 'jane', value: 'smith'}
   ]
   assert.deepEqual(classify(arr, (a, b) => a.name === b.name), [
     [
       {name: 'foo', value: 'hello'},
       {name: 'foo', value: 'world'}
     ],
-    {name: 'beep', value: 'boop'},
-    {name: 'bar', value: 'beep'}
+    [
+      {name: 'beep', value: 'boop'},
+      {name: 'beep', value: 'beep'}
+    ],
+    {name: 'bar', value: 'beep'},
+    {name: 'john', value: 'doe'},
+    {name: 'jane', value: 'smith'}
   ])
 })
